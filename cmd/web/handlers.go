1a97d7fa7191c4bf6f7e8d1a8c00646f5b0dd487
Siddhesh Suryawanshi <siddheshsuryawanshi@Siddheshs-MacBook-Pro.local>
2025-02-16 01:12:54 +0530

    Implemented session management

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,18 +5,18 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"      // New import
-	"unicode/utf8" // New import
 
+	// New import
 	"github.com/julienschmidt/httprouter"
 	"snippetbox.siddheshsuryawanshi.net/internal/models"
+	"snippetbox.siddheshsuryawanshi.net/internal/validator"
 )
 
 type snippetCreateForm struct {
-	Title       string
-	Content     string
-	Expires     int
-	FieldErrors map[string]string
+	Title               string `form:"title"`
+	Content             string `form:"content"`
+	Expires             int    `form:"expires"`
+	validator.Validator `form:"-"`
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -76,57 +76,38 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 
-	err := r.ParseForm()
+	var form snippetCreateForm
+
+	err := app.decodePostForm(r, &form)
 
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
-	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
-	if err != nil {
-		app.clientError(w, http.StatusBadRequest)
-		return
-	}
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
 
-	form := snippetCreateForm{
-		Title:       r.PostForm.Get("title"),
-		Content:     r.PostForm.Get("content"),
-		Expires:     expires,
-		FieldErrors: map[string]string{},
-	}
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters log")
 
-	// checking valid title
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		form.FieldErrors["title"] = "The field cannot be more than 100 characters"
-	}
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
 
-	// checking valid content
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "This field cannot be blank"
-	}
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal to 1, 7, 365")
 
-	// checking valid expire
-	if form.Expires != 1 && expires != 7 && expires != 365 {
-		form.FieldErrors["expired"] = "This field must equal 1, 7 or 365"
-	}
-
-	if len(form.FieldErrors) > 0 {
+	if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, http.StatusUnprocessableEntity, "create.tmpl", data)
 		return
 	}
 
-	// Pass the data to the SnippetModel.Insert() method, receiving the
-	// ID of the new record back.
 	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Snippet sucessfully created!")
+
 	// Redirect the user to the relevant page for the snippet.
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 
